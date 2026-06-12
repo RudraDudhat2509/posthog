cecb40ed1a024a5118a587af1b83b204af179168
Tom Owers <owerstom@gmail.com>
2026-06-12 15:02:06 +0100

    chore(data-warehouse): regenerate schema_enums for instagram source kind
    Generated-By: PostHog Code
    Task-Id: 1d8add04-ba73-4256-a085-96d5aed9add0

--- posthog/schema_enums.py
+++ posthog/schema_enums.py
@@ -1696,6 +1696,7 @@ class IntegrationKind(StrEnum):
     LINKEDIN_ADS = "linkedin-ads"
     SNAPCHAT = "snapchat"
     STRIPE = "stripe"
+    INSTAGRAM = "instagram"
     INTERCOM = "intercom"
     EMAIL = "email"
     TWILIO = "twilio"
