47411c2451f38bd235e6628058a73b0a05b40a65
Tom Owers <owerstom@gmail.com>
2026-06-12 14:52:13 +0100

    chore(data-warehouse): ruff format instagram migration
    Generated-By: PostHog Code
    Task-Id: 1d8add04-ba73-4256-a085-96d5aed9add0

--- posthog/migrations/1219_alter_integration_kind_instagram.py
+++ posthog/migrations/1219_alter_integration_kind_instagram.py
@@ -4,7 +4,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("posthog", "1218_team_ingested_production_event_last_checked_at"),
     ]
